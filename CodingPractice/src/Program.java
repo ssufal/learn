49e43adca3f4741cfe6eeb589386421ed39e4d6a
Mohammad Shafiullah <ssufal@gmail.com>
2021-07-22 02:51:34 +0200

    Adding prompt

--- CodingPractice/src/Program.java
+++ CodingPractice/src/Program.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[ ] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Please type the width of the Square and then Enter:");
         int x = sc.nextInt();
-        int y = sc.nextInt();
-
         Square a = new Square(x);
-        Circle b = new Circle(y);
+        System.out.print("Area of the Square is: ");
         a.area();
+        System.out.println("Please type the radius of the Circle and then Enter:");
+        int y = sc.nextInt();
+        Circle b = new Circle(y);
+        System.out.print("Area of the Circle is: ");
         b.area();
     }
 }
